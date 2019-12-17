d7bcd50f7273b8b35b2ebcf1d6bd585a7e3173cc
larryback <41230266+larryback@users.noreply.github.com>
2019-12-18 01:04:57 +0900

    12월 18일 새벽

---
+++ python_02.py
@@ -0,0 +1,5 @@
+for x in range(10):
+    print(x)
+    print('위 숫자를 제곱하면?')
+    print(x**2)
+
---
+++ python_03.py
@@ -0,0 +1,10 @@
+var_1 = '파이썬 객체지향 이야기'
+var_2 = '파이썬 객체지향 이야기'
+
+var_1 == var_2
+
+var_1 is var_2
+
+var_1.replace('파이썬', 'Python')
+
+print(var_1)
